6c0117948b0828e80baa97ee479d0ff00cf40e79
Daniel Lozada <37337887+adazol123@users.noreply.github.com>
2023-08-22 23:31:18 +0800

    initialize firebase configuration

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getPerformance, trace } from "firebase/performance";
+import { getRemoteConfig } from "firebase/remote-config"
+import { firebaseConfig } from "./index";
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
+export const remoteConfig = getRemoteConfig(app)
+export const perf = getPerformance(app);
+export const trc = trace;
