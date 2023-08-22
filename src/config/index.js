26530944b84e9f4dd8bbd9eca6a764533f99d4da
Daniel Lozada <37337887+adazol123@users.noreply.github.com>
2023-08-22 23:32:59 +0800

    initialize firebase configuration (#2)

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+const FB_KEY_DEV = import.meta.env.FIREBASE_KEY_DEV
+
+export const firebaseConfig = {
+  apiKey: FB_KEY_DEV,
+  authDomain: "supafaya-dev-app.firebaseapp.com",
+  projectId: "supafaya-dev-app",
+  storageBucket: "supafaya-dev-app.appspot.com",
+  messagingSenderId: "215596576325",
+  appId: "1:215596576325:web:7b92e3e2f00520beaabd2c",
+  measurementId: "G-MG01S5QZZ0",
+}
